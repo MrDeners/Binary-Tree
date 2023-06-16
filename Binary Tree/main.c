fd050a3df1d168a0603875ac806830090b6131f1
MrDeners <112748231+MrDeners@users.noreply.github.com>
2023-06-16 12:30:02 +0300

    Initial Commit

---
+++ Binary Tree/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Binary Tree
+//
+//  Created by MrDeners on 16.06.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

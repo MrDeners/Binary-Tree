5189b72d84b83923cf17b6d3657cd74f3bd3462a
MrDeners <112748231+MrDeners@users.noreply.github.com>
2023-06-16 12:59:26 +0300

    Push

--- Binary Tree/main.c
+++ Binary Tree/main.c
@@ -8,4 +8,45 @@
 //}
 
 
-hello
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+void push (Node** head, int data) {                             //добавление элемента в бинарное дерево
+    Node* tempNode = malloc(sizeof(Node));
+    tempNode->data = data;
+    tempNode->left = NULL;
+    tempNode->right = NULL;
+    
+    if (*head == NULL) {
+        *head = tempNode;
+        return;
+    }
+    
+    if ((*head)->data == data) return;
+    
+    Node* current = *head;
+    
+    for (int i = 0; ; i++) {
+        
+        if (data == current->data) return;
+        
+        if (data < current->data) {
+            if (current->left == NULL) {
+                current->left = tempNode;
+                return;
+            }
+            current = current->left;
+        }
+        
+        else if (data > current->data) {
+             if (current->right == NULL) {
+                 current->right = tempNode;
+                 return;
+             }
+             current = current->right;
+        }
+    }
+}
